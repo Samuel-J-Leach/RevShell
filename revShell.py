90f21552186318111a02bf704a32cc608d5bd309
flything45 <flything45@gmail.com>
2024-09-25 12:35:40 +0100

    added prototype code for controller program

---
+++ revShell.py
@@ -0,0 +1,19 @@
+import socket
+#from cryptography.hazmat.primitives.asymmetric import rsa
+
+SELF = socket.gethostbyname(socket.gethostname())
+PORT = 5050
+BUFFER_SIZE = 1024
+FORMAT = "utf-8"
+SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+SOCKET.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+SOCKET.bind((SELF, PORT))
+
+HOST = input("host IP address: ")
+SOCKET.connect((HOST, PORT))
+
+while True:
+    command = input(">> ")
+    if command == "exit":
+        break
+    SOCKET.send(bytes(command))
